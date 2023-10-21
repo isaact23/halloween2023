cb6b4fb916df27383e652c310ced7f3476ff8302
Isaac Thompson <shufflesninja@gmail.com>
2023-10-20 22:20:18 -0400

    Add face reader class

---
+++ src/python/face_recording/face_reader.py
@@ -0,0 +1,60 @@
+from collections import OrderedDict
+import cv2, dlib, imutils
+import numpy as np
+from imutils import face_utils
+
+FACIAL_LANDMARKS_IDXS = OrderedDict([
+	("mouth", (48, 68)),
+	("right_eyebrow", (17, 22)),
+	("left_eyebrow", (22, 27)),
+	("right_eye", (36, 42)),
+	("left_eye", (42, 48)),
+	("nose", (27, 35)),
+	("jaw", (0, 17))
+])
+
+# Class for interpreting the face picture and getting values.
+class FaceReader:
+
+  def __init__(self, shape_predictor):
+    self.detector = dlib.get_frontal_face_detector()
+    self.predictor = dlib.shape_predictor(shape_predictor)
+
+  def readFace(self, frame):
+    scaled = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(scaled, cv2.COLOR_BGR2GRAY)
+    rects = self.detector(gray, 1)
+
+    # Only proceed if exactly one face is detected
+    if len(rects) != 1:
+      return None
+    
+    rect = rects[0]
+
+    # determine the facial landmarks for the face region, then
+    # convert the landmark (x, y)-coordinates to a NumPy array
+    shape = self.predictor(gray, rect)
+    shape = face_utils.shape_to_np(shape)
+
+    LEFT_EYE_IDXS  = face_utils.FACIAL_LANDMARKS_IDXS.get("left_eye")
+    RIGHT_EYE_IDXS = face_utils.FACIAL_LANDMARKS_IDXS.get("right_eye")
+
+    clone = gray.copy()
+    (x, y, w, h) = cv2.boundingRect(np.array([shape[LEFT_EYE_IDXS[0] : LEFT_EYE_IDXS[1]]]))
+    leftEye = clone[y:y + h, x:x + w]
+    leftEyeResized = imutils.resize(leftEye, width=250, inter=cv2.INTER_CUBIC)
+    retval, leftEyeThreshold = cv2.threshold(leftEyeResized, 60, 255, cv2.THRESH_BINARY_INV)
+        
+    # visualize all facial landmarks with a transparent overlay
+    black = np.zeros(scaled.shape, np.uint8)
+    black[:] = (0, 0, 0)
+    output = face_utils.visualize_facial_landmarks(black, shape)
+    return {
+      "image": output,
+      "leftEye": leftEyeResized
+    }
+  
+  def readEye(self, roi):
+    _, threshold = cv2.threshold(roi, 20, 255, cv2.THRESH_BINARY_INV)
+    return threshold
+  
--- src/python/face_recording/face_recorder.py
+++ src/python/face_recording/face_recorder.py
@@ -4,102 +4,27 @@
 # https://github.com/davisking/dlib-models
 # https://www.youtube.com/watch?v=kbdbZFT9NQI&t=959s
 
-from collections import OrderedDict
-from imutils import face_utils
-import cv2, dlib, imutils, argparse
-import numpy as np
-
-CROP_LEFT = 300
-CROP_TOP = 100
-CROP_RIGHT = 470
-CROP_BOTTOM = 350
-
-FACIAL_LANDMARKS_IDXS = OrderedDict([
-	("mouth", (48, 68)),
-	("right_eyebrow", (17, 22)),
-	("left_eyebrow", (22, 27)),
-	("right_eye", (36, 42)),
-	("left_eye", (42, 48)),
-	("nose", (27, 35)),
-	("jaw", (0, 17))
-])
+import cv2, argparse
+from face_reader import FaceReader
 
 COLORS = [(19, 199, 109), (79, 76, 240), (230, 159, 23),
-        (168, 100, 168), (158, 163, 32),
-        (163, 38, 32), (180, 42, 220)]
+  (168, 100, 168), (158, 163, 32),
+  (163, 38, 32), (180, 42, 220)]
 
+# Class for recording face values into a file that can be
+# read by Node2 (The Nun) for servo movement.
 class FaceRecorder:
 
   def __init__(self):
-    self.faceClassifier = cv2.CascadeClassifier(
-      cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
-    )
 
     ap = argparse.ArgumentParser()
     ap.add_argument("-p", "--shape-predictor", required=True,
       help="path to facial landmark predictor")
     args = vars(ap.parse_args())
-    print(args["shape_predictor"])
+    print(args)
 
-    self.detector = dlib.get_frontal_face_detector()
-    self.predictor = dlib.shape_predictor(args["shape_predictor"])
+    self.faceReader = FaceReader(args["shape_predictor"])
 
-  def readFace(self, image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    rects = self.detector(gray, 1)
-    if len(rects) == 0:
-      return None
-    
-    rect = rects[0]
-
-    # determine the facial landmarks for the face region, then
-    # convert the landmark (x, y)-coordinates to a NumPy array
-    shape = self.predictor(gray, rect)
-    shape = face_utils.shape_to_np(shape)
-
-    # loop over the face parts individually
-    for (name, (i, j)) in face_utils.FACIAL_LANDMARKS_IDXS.items():
-
-      # clone the original image so we can draw on it, then
-      # display the name of the face part on the image
-      clone = image.copy()
-      cv2.putText(clone, name, (10, 30), cv2.FONT_HERSHEY_SIMPLEX,
-        0.7, (0, 0, 255), 2)
-      
-      # loop over the subset of facial landmarks, drawing the
-      # specific face part
-      for (x, y) in shape[i:j]:
-        cv2.circle(clone, (x, y), 1, (0, 0, 255), -1)
-      
-      # extract the ROI of the face region as a separate image
-      (x, y, w, h) = cv2.boundingRect(np.array([shape[i:j]]))
-      roi = image[y:y + h, x:x + w]
-      roi = imutils.resize(roi, width=250, inter=cv2.INTER_CUBIC)
-
-      #cv2.imshow("ROI", roi)
-      #cv2.imshow("Image", clone)
-      #cv2.waitKey(0)
-
-      """if name == "left_eye":
-        cv2.imshow("ROI", roi)
-        eyeRoi = self.readEye(roi)
-
-        cv2.imshow("Eye ROI", eyeRoi)
-        cv2.imshow("Image", clone)
-        cv2.waitKey(0)"""
-
-      #elif name == "right_eye":
-      #  pass
-
-      # show the particular face part
-        
-    # visualize all facial landmarks with a transparent overlay
-    output = face_utils.visualize_facial_landmarks(image, shape)
-    return output
-  
-  def readEye(self, roi):
-    _, threshold = cv2.threshold(roi, 20, 255, cv2.THRESH_BINARY_INV)
-    return threshold
   
   def record(self):
     cap = cv2.VideoCapture(0)
@@ -111,15 +36,14 @@ class FaceRecorder:
         print("Video capture failed")
         break
 
-      frame = frame[CROP_TOP:CROP_BOTTOM, CROP_LEFT:CROP_RIGHT]
-      
-      """frame = imutils.resize(frame, width=500)
+      #frame = frame[CROP_TOP:CROP_BOTTOM, CROP_LEFT:CROP_RIGHT]
 
-      output = self.readFace(frame)
-      if output is None:
-        output = frame"""
-
-      cv2.imshow("Face Recorder (Halloween 2023)", frame)
+      output = self.faceReader.readFace(frame)
+      if output is not None:
+        cv2.imshow("Face Recorder (Halloween 2023)", output["image"])
+        cv2.imshow("Left Eye", output["leftEye"])
+      else:
+        cv2.imshow("Face", frame)
       
       if (cv2.waitKey(1) & 0xFF == ord("q")):
         print("Quitting program")
